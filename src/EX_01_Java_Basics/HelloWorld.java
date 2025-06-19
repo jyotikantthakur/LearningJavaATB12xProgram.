d3e90b178bfd53026d765e133d5651734f3ce8e4
Jyoti Kant Thakur <jyotikantthakur@gmail.com>
2025-06-20 01:35:43 +0530

    push1

---
+++ src/EX_01_Java_Basics/HelloWorld.java
@@ -0,0 +1,19 @@
+package EX_01_Java_Basics;
+
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello Jyoti, Welcome to my 1st Java Class");
+
+    }
+
+
+
+
+
+
+}
+
---
+++ src/EX_01_Java_Basics/Lab001_HelloJyoti.java
@@ -0,0 +1,9 @@
+package EX_01_Java_Basics;
+
+public class Lab001_HelloJyoti {
+    public static void main(String[] args) {
+        System.out.println("Hello Aishu come lets learn Java together");
+    }
+
+}
+
